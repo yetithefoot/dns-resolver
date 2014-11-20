a1e034b91c02c1b643b69fb7b17f202c7bb82bc4
Vlad Tsepelev <reg.yeti@gmail.com>
2014-11-20 03:11:02 +0200

    add examples

--- dns-resolver.js
+++ dns-resolver.js
@@ -6,9 +6,8 @@ exports.ips; // will keep own domain ips afret setup
 
 /**
  * Will configure module with domain name and app ips.
- * @param  {[type]}   selfDomain [description]
- * @param  {Function} done       [description]
- * @return {[type]}              [description]
+ * @param  {[type]}   selfDomain Domain to configure with.
+ * @param  {Function} done(err, ips) Callback function.
  */
 exports.configure = function(selfDomain, done){
 	exports.SELF_DOMAIN = selfDomain;
@@ -27,42 +26,40 @@ exports.configure = function(selfDomain, done){
 			return done && done(err);
 		}
 
-		console.log(exports.SELF_DOMAIN + ' addresses populated: ' + JSON.stringify(addresses));
+		//console.log(exports.SELF_DOMAIN + ' addresses populated: ' + JSON.stringify(addresses));
 		exports.ips = addresses;
-		done && done(null, exports.ips);
+		done && done(null, {domain:selfDomain, ips:exports.ips});
 	})
 }
 
 /**
  * Check that passed domain points to domain passed to configure().
- * @param  {[type]}   domain [description]
- * @param  {Function} done   [description]
- * @return {Boolean}         [description]
+ * @param  {[type]}   domain Domain to check.
+ * @param  {Function} done   Callback(err, ip). 
  */
-exports.isSamePointed = function(domain, done){
+exports.resolve = function(domain, done){
 	if(!exports.SELF_DOMAIN) return done && done(new Error('Module not configured. Run configure method first'))
 	if(!exports.ips) return done && done(new Error('There are no ips for configured domain '+exports.SELF_DOMAIN))
 	dns.resolve(domain, function (err, addresses) {
 		if (err) {
 			err.message = 'Cant populate DNS record for ' + domain;
 			err.code = 'norecord'
-			console.log(err);
+			// console.log(err);
 			return done && done(err);
 		}
 		addresses = addresses || [];
 		if(!addresses.length) {
 			err.message = 'DNS record for domain '+domain +' not found. Make sure that record exists and wait 24 hours.';
 			err.code = 'norecord'
-			console.log(err);
+			// console.log(err);
 			return done && done(err);
 		}
 
-		console.log(domain + ' addresses: ' + JSON.stringify(addresses));
+		//console.log(domain + ' addresses: ' + JSON.stringify(addresses));
 
 		// ensure that at least one of addresses points to
 		var matched = false;
 		addresses.forEach(function(address){
-
 			if(exports.ips.indexOf(address) !== -1){
 				matched = address;
 			}
@@ -72,9 +69,9 @@ exports.isSamePointed = function(domain, done){
 			return done && done(null, matched);
 		}else{
 
-			var err = new Error('DNS record for '+domain +' points to another ip adress');
+			var err = new Error('DNS record for '+domain +' points to another ip adress ['+addresses+']');
 			err.code = 'anotherip'
-			console.log(err);
+			// console.log(err);
 			return done && done(err, addresses)
 		}
 	})
---
+++ example/example1.js
@@ -0,0 +1,32 @@
+var resolver = require('../dns-resolver')
+
+
+// configure resolver
+var SELF_DOMAIN = 'npmjs.org'
+resolver.configure(SELF_DOMAIN, function(err, data){
+	console.log('Resolver configured with domain %s: ', data.domain, data.ips)
+
+	// check that domain 
+	var DOMAIN_TO_RESOLVE = 'npmjs.org'
+	resolver.resolve(DOMAIN_TO_RESOLVE, function(err, data){
+		if (err) console.log(err)
+
+		console.log(data)
+	})
+
+	var DOMAIN_TO_RESOLVE = 'github.com'
+	resolver.resolve(DOMAIN_TO_RESOLVE, function(err, data){
+		if (err) {
+			return console.log(err)
+		}
+
+		console.log(data)
+	})
+
+})
+
+
+
+
+
+
